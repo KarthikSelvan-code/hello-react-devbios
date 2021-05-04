3f206bc46730961e6a2eeab659ec4a0c71543253
KarthikSelvan-code <58622294+KarthikSelvan-code@users.noreply.github.com>
2021-05-04 20:32:18 +0530

    DevBio

---
+++ src/DeveloperBio.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function DeveloperBio() {
+    return (
+        <div className="card d-inline-block text-dark">
+            <div className="card-body">
+                <h5 className="card-title">Karthik Palanivelu</h5>
+                <p className="card-text">
+                    <span className="bold">Favorite Language :</span> Java
+                </p>
+                <p className="card-text">
+                    <span className="bold">Year Started :</span> 2014
+                </p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default DeveloperBio
+
